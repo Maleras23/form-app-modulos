3c93d26d9a453b8c51714b05785fa585af0d4af4
Carlos Calzadilla <maleras24@gmail.com>
2025-12-10 13:01:13 +0000

    Register page done

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators'
 
 @Component({
   selector: 'app-register-page.component',
@@ -6,4 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './register-page.component.html',
   styles: ``,
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+
+  public myForm: FormGroup;
+
+  constructor( private fb: FormBuilder){
+    this.myForm = this.fb.group({
+      name: ['', [ Validators.required, Validators.pattern( customValidators.firstNameAndLastnamePattern)] ],
+      email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ] ],
+      username: ['', [ Validators.required, customValidators.cantBeStrider ] ],
+      password: ['', [ Validators.required, Validators.minLength(6) ] ],
+      password2: ['', [ Validators.required ] ],
+    })
+  }
+
+  inValidField ( field: string ){
+    // todo: obtener validacion desde un serviciob
+  }
+
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+  }
+
+}
